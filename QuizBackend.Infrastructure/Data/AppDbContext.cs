f8309dc90fbe5079467bc4fa0631e57ba77ed222
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-08-02 12:13:43 +0200

    added docker yaml, and userdbcontext

---
+++ QuizBackend.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = 127.0.0.1; Database = QuizBackendDatabase; user id = SA; password = Pass@word; Encrypt = false; TrustServerCertificate = true; Integrated Security = false;");
+        }
+    }
+}
