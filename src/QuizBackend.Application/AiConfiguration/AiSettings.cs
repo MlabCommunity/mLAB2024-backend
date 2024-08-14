820ac833af63a33403013d215fb05ceef7a3617c
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-08-14 14:34:06 +0200

    base structure for contract

---
+++ src/QuizBackend.Application/AiConfiguration/AiSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend.Application.AiConfiguration
+{
+    public class AiSettings
+    {
+        public required string Provider { get; set; }
+        public OpenAISettings OpenAI { get; set; }
+        public AzureOpenAISettings AzureOpenAI { get; set; }
+        public GeminiSettings Gemini { get; set; }
+    }
+}
---
+++ src/QuizBackend.Application/AiConfiguration/AzureOpenAISettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend.Application.AiConfiguration
+{
+    public class AzureOpenAISettings
+    {
+        public required string Endpoint { get; set; }
+        public required string ApiKey { get; set; }
+        public required string ModelId { get; set; }
+    }
+}
