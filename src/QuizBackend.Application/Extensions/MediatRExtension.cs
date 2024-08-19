4496abce2cebf70d816e809d192c4a808bedcaf0
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-08-20 00:27:58 +0200

    Structure for cqrs (#11)

---
+++ src/QuizBackend.Application/Extensions/MediatRExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend.Application.Extensions
+{
+    public static class MediatRExtension
+    {
+        public static void AddMediatRExtension(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
