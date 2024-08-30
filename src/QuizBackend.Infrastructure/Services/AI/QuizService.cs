8ca126d2915dbf4cc2c3c222230f2139fae01feb
Janek Barnat <janbarn00@gmail.com>
2024-08-30 13:24:55 +0200

    add log warning for quiz generation failed

--- src/QuizBackend.Infrastructure/Services/AI/QuizService.cs
+++ src/QuizBackend.Infrastructure/Services/AI/QuizService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
 using Newtonsoft.Json;
 using QuizBackend.Application.Commands.Quizzes.GenerateQuiz;
 using QuizBackend.Application.Dtos.Quizzes.CreateQuiz;
@@ -11,30 +12,36 @@ namespace QuizBackend.Infrastructure.Services.AI
     public class QuizService : IQuizService
     {
         private readonly IKernelService _kernelService;
+        private readonly ILogger<QuizService> _logger;
 
-        public QuizService(IKernelService kernelService)
+        public QuizService(IKernelService kernelService, ILogger<QuizService> logger)
         {
             _kernelService = kernelService;
+            _logger = logger;
         }
-
         public async Task<CreateQuizDto> GenerateQuizFromPromptTemplateAsync(GenerateQuizCommand command)
         {
             var kernelArguments = new KernelArguments
-            {
-                {"content", command.Content },
-                {"numberOfQuestions", command.NumberOfQuestions},
-                {"typeOfQuestions", command.QuestionType}
-            };
+        {
+            {"content", command.Content },
+            {"numberOfQuestions", command.NumberOfQuestions},
+            {"typeOfQuestions", command.QuestionType}
+        };
 
             var jsonResponse = await _kernelService.CreatePluginFromPromptDirectory("GenerateQuiz", kernelArguments);
+            var quizDto = JsonConvert.DeserializeObject<CreateQuizDto>(jsonResponse);
 
-            if (string.IsNullOrWhiteSpace(jsonResponse))
+            if (string.IsNullOrWhiteSpace(jsonResponse) || quizDto is null)
             {
-                throw new BadRequestException("Try generate again");
+
+                _logger.LogWarning("Quiz generation failed: Content: {Content}, NumberOfQuestions: {NumberOfQuestions}, TypeOfQuestions: {TypeOfQuestions}",
+                    command.Content, command.NumberOfQuestions, command.QuestionType);
+
+                throw new BadRequestException("Try generating again");
             }
 
-            var quizDto = JsonConvert.DeserializeObject<CreateQuizDto>(jsonResponse);
             return quizDto;
         }
     }
+
 }
