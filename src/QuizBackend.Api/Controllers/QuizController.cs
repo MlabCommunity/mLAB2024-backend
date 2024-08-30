e153031b39d8ca63fd5ce59f21ebd57f704443bc
Janek <84191665+bulvin@users.noreply.github.com>
2024-08-30 11:57:01 +0200

    Remove Quiz (#32)
    * resolve conflict
    
    * del whitespaces & change to NotFoundException
    
    * clean whitespaces
    
    * clean whitespaces again
    
    * clean whitespace in delete command handler

--- src/QuizBackend.Api/Controllers/QuizController.cs
+++ src/QuizBackend.Api/Controllers/QuizController.cs
@@ -12,9 +12,8 @@ using QuizBackend.Application.Dtos.Quizzes.GenerateQuiz;
 using QuizBackend.Application.Commands.Quizzes.UpdateStatusQuiz;
 using QuizBackend.Domain.Enums;
 using QuizBackend.Application.Commands.UpdateStatusQuiz;
+using QuizBackend.Application.Commands.Quizzes.DeleteQuiz;
 using QuizBackend.Application.Commands.Quizzes.UpdateAvailability;
-
-
 
 namespace QuizBackend.Api.Controllers
 {
@@ -28,18 +27,14 @@ namespace QuizBackend.Api.Controllers
             _mediator = mediator;
         }
 
-        
         [HttpPost("generate-quiz")]
         [SwaggerOperation(Summary = "Generating Quiz with questions and anserws", Description = "QuestionType: MultipleChoices = 0, TrueFalse = 1")]
         [ProducesResponseType(typeof(GenerateQuizDto), StatusCodes.Status200OK)]
         public async Task<IActionResult> GenerateQuizFromPromptTemplateAsync(GenerateQuizCommand command)
         {
             var result = await _mediator.Send(command);
-
             return Ok(result);
         }
-
-        
 
         [HttpGet("{Id}")]
         [SwaggerOperation(
@@ -67,24 +62,19 @@ namespace QuizBackend.Api.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<PagedDto<QuizDto>>> GetPagedQuizzes([FromQuery] GetPagedQuizzesQuery query, CancellationToken cancellation)
         {
-
             var result = await _mediator.Send(query);
             return Ok(result);
         }
 
-        
         [HttpPost("create-quiz")]
         [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateQuiz(CreateQuizCommand command)
         {
             var quizId = await _mediator.Send(command);
-
             return Ok(quizId);
-
         }
 
-       
         [HttpPatch("{id}/status")]
         [SwaggerOperation(
             Summary = "Update the status of a quiz",
@@ -100,7 +90,6 @@ namespace QuizBackend.Api.Controllers
             return Ok(response);
         }
 
-        
         [HttpPatch("{id}/availability")]
         [SwaggerOperation(
            Summary = "Update the availability of a quiz",
@@ -115,6 +104,22 @@ namespace QuizBackend.Api.Controllers
             var command = new UpdateAvailabilityCommand(id, availability);
             var response = await _mediator.Send(command);
             return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerOperation(
+           Summary = "Delete a quiz",
+           Description = "Deletes a quiz based on its ID."
+        )]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteQuiz([FromRoute] Guid id)
+        {
+            var command = new DeleteQuizCommand(id);
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
