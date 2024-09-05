9e90dd650d4831c48472a05a091240e1ac7d930f
Janek Barnat <janbarn00@gmail.com>
2024-09-05 09:57:40 +0200

    convert to file scoped

--- src/QuizBackend.Infrastructure/Extensions/ProcessorsExtensions.cs
+++ src/QuizBackend.Infrastructure/Extensions/ProcessorsExtensions.cs
@@ -2,13 +2,11 @@
 using QuizBackend.Application.Interfaces;
 using QuizBackend.Infrastructure.Services.Processors;
 
-namespace QuizBackend.Infrastructure.Extensions
+namespace QuizBackend.Infrastructure.Extensions;
+public static class ProcessorsExtensions
 {
-    public static class ProcessorsExtensions
+    public static void AddProcessors(this IServiceCollection services)
     {
-        public static void AddProcessors(this IServiceCollection services)
-        {
-            services.AddSingleton<IAttachmentProcessor, AttachmentProcessor>();
-        }
+        services.AddSingleton<IAttachmentProcessor, AttachmentProcessor>();
     }
 }
