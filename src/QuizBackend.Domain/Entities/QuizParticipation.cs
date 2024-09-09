19ce317c64340650f4f0d8b0ab1b023392542b3a
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-09-09 09:53:31 +0200

    added properties to guest user

--- src/QuizBackend.Domain/Entities/QuizParticipation.cs
+++ src/QuizBackend.Domain/Entities/QuizParticipation.cs
@@ -7,8 +7,9 @@ public class QuizParticipation : BaseEntity
 {
     public Guid QuizId { get; set; }
     public Quiz Quiz { get; set; } = null!;
-    public string ParticipantId { get; set; } = null!;
-    public User Participant { get; set; } = null!;
+    public string? ParticipantId { get; set; }
+    public User? Participant { get; set; }
+    public string? GuestParticipant { get; set; }
     public DateTime ParticipationDateUtc { get; set; }
     public QuizParticipationStatus Status { get; set; } = QuizParticipationStatus.Started;
     public QuizResult? QuizResult { get; set; }
---
+++ src/QuizBackend.Infrastructure/Migrations/20240909073442_FinalQuizParticipantEntities.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalQuizParticipantEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ParticipantId",
+                table: "QuizzesParticipations",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GuestParticipant",
+                table: "QuizzesParticipations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GuestParticipant",
+                table: "QuizzesParticipations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParticipantId",
+                table: "QuizzesParticipations",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
