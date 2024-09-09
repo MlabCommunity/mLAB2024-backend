1f31fbb39d17c477e2392820f3cd0401793c3c4f
Janek <84191665+bulvin@users.noreply.github.com>
2024-09-09 10:14:13 +0200

    Attachment Processing strategy (#42)
    * add generate quiz from attachments
    
    * improvements to quiz generation
    
    * del whitespace
    
    * convert to file scoped namespace
    
    * convert to file scoped
    
    * resolve conflicts
    
    * add strategy pattern for processing attachments [pdf, txt, docx, pptx, xlsx]
    
    * resolve conflict
    
    * Add blank lines and string.empty
    
    * add allowed extension - pptx

---
+++ src/QuizBackend.Infrastructure/Services/Processors/Attachments/PptxProcessingStrategy.cs
@@ -0,0 +1,31 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Http;
+using QuizBackend.Infrastructure.Interfaces;
+using System.Text;
+
+namespace QuizBackend.Infrastructure.Services.Processors.Attachments;
+
+public class PptxProcessingStrategy : IAttachmentProcessingStrategy
+{
+    public async Task<string> ProcessFile(IFormFile file)
+    {
+        using var memoryStream = new MemoryStream();
+        await file.CopyToAsync(memoryStream);
+        memoryStream.Position = 0;
+
+        var text = new StringBuilder();
+
+        using (PresentationDocument presentationDocument = PresentationDocument.Open(memoryStream, false))
+        {
+            foreach (SlidePart slidePart in presentationDocument.PresentationPart!.SlideParts)
+            {
+                foreach (var textElement in slidePart.Slide.Descendants<DocumentFormat.OpenXml.Drawing.Text>())
+                {
+                    text.AppendLine(textElement.Text);
+                }
+            }
+        }
+
+        return text.ToString();
+    }
+}
