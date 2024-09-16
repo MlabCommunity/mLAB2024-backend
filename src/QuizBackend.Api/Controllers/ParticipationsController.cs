61ea9c764fe353744f4451d7a3092e7b19919a0a
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-09-16 13:41:39 +0200

    Quiz result feature (#50)
    * first entities to take quiz function
    
    * fix structure
    
    * Ok
    
    * first structure
    
    * Delete src/QuizBackend.Infrastructure/Migrations/20240906085435_QuizResultEntities.cs
    
    * Delete src/QuizBackend.Infrastructure/Migrations/20240906085435_QuizResultEntities.Designer.cs
    
    * added return quzi return with details quiz and command to send user answers from quiz
    
    * Added StopQuizCommand
    
    * fix refreshToken with return status code
    
    * deleted useless migration
    
    * merged with develop
    
    * fix merge

--- src/QuizBackend.Api/Controllers/ParticipationsController.cs
+++ src/QuizBackend.Api/Controllers/ParticipationsController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using QuizBackend.Application.Commands.QuizzesParticipations.StopQuiz;
+using QuizBackend.Application.Commands.QuizzesParticipations.SubmitQuizAnswer;
+using QuizBackend.Application.Queries.QuizzesParticipations.GetQuizResult;
 using QuizBackend.Application.Commands.QuizzesParticipations.JoinQuiz;
 using QuizBackend.Application.Queries.Quizzes.GetQuizParticipation;
 using Swashbuckle.AspNetCore.Annotations;
@@ -51,5 +54,32 @@ public class ParticipationsController : BaseController
         var query = new GetQuizParticipationQuery(id);
         var result = await _mediator.Send(query);
         return Ok(result);
+    }
+
+    [HttpPost("submit")]
+    [SwaggerOperation(Summary = "Submit quiz participation, save user answer and calculate score.")]
+    public async Task<IActionResult> SubmitQuizParticipation(SubmitQuizAnswerCommand command)
+    {
+        await _mediator.Send(command);
+        return NoContent();
+    }
+
+    [HttpGet("/result/{quizParticipationId}")]
+    [SwaggerOperation(Summary = "Get Quiz result from quizParticipationId")]
+    [ProducesResponseType(typeof(QuizResultResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetQuizResult(Guid quizParticipationId)
+    {
+        var query = new GetQuizResultQuery(quizParticipationId);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpPatch("{participationId:guid}/stop")]
+    public async Task<IActionResult> StopQuiz(Guid participationId)
+    {
+        var command = new StopQuizCommand(participationId);
+        await _mediator.Send(command);
+
+        return NoContent();
     }
 }
