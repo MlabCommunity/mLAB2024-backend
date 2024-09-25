ca171308d4e57ef8812134052a613ec3c38319f6
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-09-25 12:44:24 +0200

    added paged information

--- src/QuizBackend.Application/Queries/Quizzes/GetQuiz/GetQuizQuery.cs
+++ src/QuizBackend.Application/Queries/Quizzes/GetQuiz/GetQuizQuery.cs
@@ -1,6 +1,7 @@
-﻿using QuizBackend.Application.Dtos.Quizzes;
+﻿using QuizBackend.Application.Dtos.Paged;
+using QuizBackend.Application.Dtos.Quizzes;
 using QuizBackend.Application.Interfaces.Messaging;
 
 namespace QuizBackend.Application.Queries.Quizzes.GetQuiz;
 
-public record GetQuizQuery(Guid Id, int? Page, int? PageSize) : IQuery<QuizDetailsDto>;
+public record GetQuizQuery(Guid Id, int? Page, int? PageSize) : IQuery<PagedDto<QuizDetailsDto>>;
