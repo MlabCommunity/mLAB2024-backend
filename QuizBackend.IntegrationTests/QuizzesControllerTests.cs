28834d726017207531def0bdd6ad4d8926d1be99
Janek Barnat <janbarn00@gmail.com>
2024-09-26 12:37:14 +0200

    setup integration tests project

---
+++ QuizBackend.IntegrationTests/QuizzesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace QuizBackend.IntegrationTests;
+
+public class QuizzesControllerTests
+{
+    [Fact]
+    public async Task GetQuiz_WithIdParameter_ReturnsOkResult()
+    {
+        // arrange
+
+        // act
+ 
+
+        // assert
+       
+    }
+}
