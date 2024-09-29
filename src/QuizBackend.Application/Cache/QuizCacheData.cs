fbeb20d7c272addadd5f352092ffe10820b48607
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-09-29 14:59:19 +0200

    Third agent ai (#77)
    * added third agent to validate json structure
    
    * deleted tests
    
    * added translated TrueFalse
    
    * deleted custom signs from polish and german language
    
    * added prompts folder with generate quiz to csproj
    
    * deleted folder include from infrastructure.csproj because caused problem with prompts
    
    * changed prompts to always copy to output directory

--- src/QuizBackend.Application/Cache/QuizCacheData.cs
+++ src/QuizBackend.Application/Cache/QuizCacheData.cs
@@ -5,5 +5,6 @@ public class QuizCacheData
     public string Content { get; set; } = null!;
     public string QuizResponse { get; set; } = null!;
     public int NumberOfQuestions { get; set; }
+    public string Language { get; set; } = null!;
     public string QuestionTypes { get; set; } = null!;
 }
